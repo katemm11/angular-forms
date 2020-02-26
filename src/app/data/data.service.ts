11a2a632cb1461c96ca3c53212384459e586c232
Murray <KATE.MURRAY@VIZIENTINC.COM>
2020-02-26 13:18:58 -0600

    finished first http request

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { UserSettings } from "./user-settings";
+import { Observable, of } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  postUserSettingsForms(userSettings: UserSettings): Observable<any> {
+    return this.http.post(
+      "https://putsreq.com/tJyMMhTIVdfn6Q96N8Hc",
+      userSettings
+    );
+
+    //return of(userSettings);
+  }
+}
